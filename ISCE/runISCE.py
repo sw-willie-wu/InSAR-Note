63bdc3ed9f11798767c33272a514ffca5937d398
Sheng Wei Wu <ab30722@gmail.com>
2019-05-30 19:57:32 +0800

    version 2.0

--- ISCE/runISCE.py
+++ ISCE/runISCE.py
@@ -2,20 +2,20 @@
 '''
 Title: ISCE processing program
 Author: S.W.
-Version: 1.0
+Version: 2.0
 
 Describe:
 The program based on python3, processing ALOS images(2000 later) with ISCE tools.
 
 '''
+
 import shutil
-import sys, os
+import sys
+import os
 import numpy as np
 import matplotlib.pyplot as plt
-#from usrfunc import *
 from datetime import datetime
 from itertools import combinations
-
 
 # Initial Data
 imageHome = '/media/willie/DDD/ALOS_AVNIR-2PALSAR_data/20161224/'
@@ -26,184 +26,250 @@ unwrapMethod = 'snaphu_mcf'
 max_daydiff = 1000
 max_perpendicular_baseline = 1500
 
-def IMG(path, polar):
+
+def runscript(path, bash_file):  # run bash file
+    os.chdir(path)
+    start = datetime.now()
+    os.system('sh '+bash_file)
+    end = datetime.now()
+    time = (end-start).seconds
+    os.system('echo " - Time taken: %f min -" >> runISCE.log' % (time/60.0))
+
+
+def imagedata(type, path, polar):  # read IMG or LED data from ALOS image folder
     files = os.listdir(path)
     for fileName in files:
-        if 'IMG-'+polar in fileName:
-            break
-    return path + '/' + fileName
+        if type+'-'+polar in fileName:
+            imageType = os.path.join(path, fileName)
+        elif type+'-ALPSRP' in fileName:
+            imageType = os.path.join(path, fileName)
+    return imageType
 
-def LED(path):
-    files = os.listdir(path)
-    for fileName in files:
-        if 'LED-' in fileName:
-            break
-    return path + '/' + fileName
 
-def xml_tmp(): #the isce xml template
+def fbd2fbs(fdb2fbs):
+    if fdb2fbs == True:
+        FBD2FBS = '<property name="RESAMPLE_FLAG">dual2single</property>'
+    else:
+        FBD2FBS = ''
+    return FBD2FBS
+
+
+def xml_tmp():  # template of insarApp,xml
     xml_tmp = """<?xml version="1.0" encoding="UTF-8"?>
 <insarApp>
     <component name="insar">
         <property name="Sensor name">ALOS</property>
-        <property name="doppler method">useDOPIQ</property>
-        <property name="posting"> 25 </property>
         <component name="master">
             <property name="IMAGEFILE">{masterIMG}</property>
             <property name="LEADERFILE">{masterLED}</property>
             <property name="OUTPUT">{masterDate}.raw</property>
+            {mFBD2FBS}
         </component>
         <component name="slave">
             <property name="IMAGEFILE">{slaveIMG}</property>
             <property name="LEADERFILE">{slaveLED}</property>
             <property name="OUTPUT">{slaveDate}.raw</property>
+            {sFBD2FBS}
         </component>
         <component name="Dem">
             <catalog>{DEM}</catalog>
         </component>
-        <property name="unwrap">True</property>
-        <property name="unwrappername">{unwrapMethod}</property>
-        <property name="geocode bounding box">[22.81040210956758, 23.835561740571222, 120.24001890274782, 121.17013224371266]</property>
-		<property name="geocode list">['filt_topophase.flat', 'los.rdr', 'topophase.cor', 'phsig.cor']</property>
     </component>
 </insarApp>"""
     return xml_tmp
 
-def runScript(path, bash_file): #run bash file
-    os.chdir(path)
-    os.system('sh '+bash_file)
 
-def steps1(imageDate, imageHome, DEM, polor, unwrapMethod): #create xml file and script for pairs
-    global root
+def xml_tmp2():  # template of insarApp,xml
+    xml_tmp = """<?xml version="1.0" encoding="UTF-8"?>
+<insarApp>
+    <component name="insar">
+        <property name="Sensor name">ALOS</property>
+        <component name="master">
+            <property name="IMAGEFILE">{masterIMG}</property>
+            <property name="LEADERFILE">{masterLED}</property>
+            <property name="OUTPUT">{masterDate}.raw</property>
+            {mFBD2FBS}
+        </component>
+        <component name="slave">
+            <property name="IMAGEFILE">{slaveIMG}</property>
+            <property name="LEADERFILE">{slaveLED}</property>
+            <property name="OUTPUT">{slaveDate}.raw</property>
+            {sFBD2FBS}
+        </component>
+        <component name="Dem">
+            <catalog>{DEM}</catalog>
+        </component>
+            <property name="unwrap">True</property>
+            <property name="unwrappername">{unwrapMethod}</property>
+            <property name="geocode list">['filt_topophase.flat']</property>
+    </component>
+</insarApp>"""
+    return xml_tmp
+
+
+def readtable(table):
+    with open(table, 'r') as f:
+        data = f.readlines()
+        row = len(data)
+        column = len(data[0].split()) + 1
+        record = np.zeros([row, column])
+        for i in range(row):
+            info = data[i].split()
+            try:
+                infoYMD = info[0].split('_')
+                record[i] = [infoYMD[0], infoYMD[1], info[1], info[2]]
+            except:
+                None
+    if sum(record[0]) == 0:
+        table = np.delete(record, 0, 0)
+    else:
+        table = record
+    return table
+
+
+def str2date(string):
+    date = datetime(int(string[:4]), int(string[4:6]), int(string[6:8]))
+    return date
+
+
+def preproc(dataDate, imageHome, DEM, polor):  # do preprocessing to get perpendicular baseline
+    global root, log
     imagePath = os.listdir(imageHome)
-    bash = open('01_calperpbl.sh', 'w')
-    bash.write('echo "- - - - start preprocessing - - - " >> '+root+'/runISCE.log\n')
+    bash = open('01_calBperp.sh', 'w')
+    os.system('echo "\n- - - - - - - - - - start preprocessing - - - - - - - - - - \n" >> %s' % log)
+
     # make perpendicular baseline folder
-    if not os.path.isdir('pbl'):
-        os.mkdir('pbl')
+    if not os.path.isdir('Bperp'):
+        os.mkdir('Bperp')
 
     # create pairs (master and slave image)
-    with open(imageDate, 'r') as f:
+    with open(dataDate, 'r') as f:
         dates = f.read().strip().split()
-    pairs = list(combinations(dates, 2))
+    pairs = [[dates[0], d2] for d2 in dates[1:]]
     for pair in pairs:
-        pair_folder = pair[0][2:] + '_' + pair[1][2:]
-        if not os.path.isdir(root + '/pbl/' + pair_folder):
-            os.mkdir(root + '/pbl/' + pair_folder)
-        os.chdir(root + '/pbl/' + pair_folder)
+        pair_folder = os.path.join(root+'/Bperp/', pair[0]+'_'+pair[1])
+        if not os.path.isdir(pair_folder):
+            os.mkdir(pair_folder)
         for i in imagePath:
             if pair[0] in i:
-                master = i
-            elif pair[1] in i:
-                slave = i
-                break
+                if i[-1] == '3':
+                    master = os.path.join(imageHome, i)
+                    mfbd2fbs = True
+                else:
+                    master = os.path.join(imageHome, i)
+                    mfbd2fbs = False
+            if pair[1] in i:
+                if i[-1] == '3':
+                    slave = os.path.join(imageHome, i)
+                    sfbd2fbs = True
+                else:
+                    slave = os.path.join(imageHome, i)
+                    sfbd2fbs = False
 
         # create xml file
-        with open('insarApp.xml', 'w') as xml:
-            xml.write(xml_tmp().format(masterIMG=IMG(imageHome+master,polor), masterLED=LED(imageHome+ \
-                master), masterDate=pair[0][2:], slaveIMG=IMG(imageHome+slave,polor), \
-                slaveLED=LED(imageHome+slave), slaveDate=pair[1][2:], DEM=DEM, \
-                unwrapMethod=unwrapMethod))
+        with open(pair_folder+'/insarApp.xml', 'w') as xml:
+            xml.write(xml_tmp().format(masterIMG=imagedata('IMG', master, polor), masterLED=imagedata('LED', master, polor), masterDate=pair[0][2:],
+                                       slaveIMG=imagedata('IMG', slave, polor), slaveLED=imagedata('LED', slave, polor), slaveDate=pair[1][2:],
+                                       DEM=DEM, mFBD2FBS=fbd2fbs(mfbd2fbs), sFBD2FBS=fbd2fbs(sfbd2fbs)))
+
+            os.system('echo " - create insarApp.xml in %s" >> %s' % (pair_folder, log))
 
         # write script to process
-        bash.write('cd ' + os.getcwd() + '\n')
-        bash.write('insarApp.py insarApp.xml --end=preprocess\n')
-        bash.write('echo "'+pair_folder+' preprocessing end" >> '+root+'/runISCE.log\n')
+        bash.write('cd %s\ninsarApp.py insarApp.xml --end=preprocess\necho " - %s preprocessing finished" >> %s\n' %
+                   (pair_folder, pair_folder, log))
     bash.close()
 
-def steps2(time, space): # give threshold
-    global root
-    if not os.path.isdir('isce_out'):
-        os.mkdir('isce_out')
-    pairs = os.listdir('pbl')
-    bash = open('02_runDInSAR.sh', 'w')
-    bash.write('echo "- - - - start processing - - - - " >> '+root+'/runISCE.log\n')
-    pbl_txt = open('perp_baseline.txt', 'w')
-    pbl_txt.write('    pair  \t\t  perp_bl_bottom \t\t  perp_bl_top  \t\t  prep_bl_ave\n')
+
+def calbperp(pairsFolder, dataDate):
+    global root, log
+    os.chdir(root)
+    dictT = {}
+    dictS = {}
+    ts = open('TStable.txt', 'w')
+    ts.write('       pair  \t\t  Temperal \t\t  Spatial  \n')
+    with open(dataDate, 'r') as f:
+        d = f.read().strip().split()
+        allpairs = list(combinations(d, 2))
+        dictT[d[0]] = 0
+        dictS[d[0]] = 0
+    pairs = os.listdir(pairsFolder)
     for pair in pairs:
         dates = pair.split('_')
-        date1 = datetime(int('20'+dates[0][:2]), int(dates[0][2:4]), int(dates[0][4:6]))
-        date2 = datetime(int('20'+dates[1][:2]), int(dates[1][2:4]), int(dates[1][4:6]))
-        diff_date = (date2-date1).days
-        with open('pbl/'+pair+'/isce.log', 'r') as f:
-        #with open('/home/willie/DATA/isce_output/'+pair+'/isce.log', 'r') as f:
-            for line in f:
-                if 'baseline.perp_baseline_bottom' in line:
-                    perp_bl_b = line[32:].strip()
-                elif 'baseline.perp_baseline_top' in line:
-                    perp_bl_t = line[29:].strip()
-                    break
-        perp_bl_ave = (float(perp_bl_b)+float(perp_bl_t)) / 2
-        pbl_txt.write('%-15s %-20s %-20s %-20s \n' %(pair, perp_bl_b, perp_bl_t, str(perp_bl_ave)))
-        if abs(perp_bl_ave) <= space and diff_date <= time:
-            shutil.copytree(root+'/pbl/'+pair, root+'/isce_out/'+pair)
-            bash.write('cd '+root+'/isce_out/'+pair+'\n')
-            bash.write('insarApp.py insarApp.xml --start=verifyDEM\n')
-            bash.write('echo "'+pair+' preprocessing end" >> '+root+'/runISCE.log\n')
-    pbl_txt.close()
+        date1 = str2date(dates[0])
+        date2 = str2date(dates[1])
+        diff_date = (date2 - date1).days
+        dictT[date2.strftime('%Y%m%d')] = diff_date
+        pair_folder = os.path.join(pairsFolder, pair)
+        with open(pair_folder+'/insarProc.xml', 'r') as f:
+            for line in f.readlines():
+                if line.find('<perp_baseline_top>') != -1:
+                    top = float(line[27:42])
+                elif line.find('<perp_baseline_bottom>') != -1:
+                    bottom = float(line[30:45])
+            dictS[date2.strftime('%Y%m%d')] = (top + bottom)/2
+    for pair in allpairs:
+        ts.write('%-20s %6d %18f \n' %
+                 (pair[0]+'_'+pair[1], dictT[pair[1]]-dictT[pair[0]], dictS[pair[1]]-dictS[pair[0]]))
+    ts.close()
+    os.system('echo " - Create TStable completed - " >> %s' % log)
+
+
+def geobox(TStable, imageHome, DEM, polor, unwrapMethod):
+    table = readtable(TStable)
+    temporal = np.sort(table[:, 2])
+    spatial = sorted(list(table[:, 3]), key=abs)
+    sortT = [list(temporal).index(i) for i in table[:, 2]]
+    sortS = [list(spatial).index(i) for i in table[:, 3]]
+    sortTS = [sortT[i]+sortS[i] for i in range(len(sortT))]
+    index = sortTS.index(min(sortTS))
+    ref_master, ref_slave = str(int(table[index, 0])), str(int(table[index, 1]))
+    if not os.path.isdir('ref'):
+        os.mkdir('ref')
+    refgeo = os.path.join('ref', ref_master+'_'+ref_slave)
+    os.mkdir(refgeo)
+    imagePath = os.listdir(imageHome)
+    for path in imagePath:
+        if path.find(ref_master) != -1:
+            if i[-1] == '3':
+                master = os.path.join(imageHome, path)
+                mfbd2fbs = True
+            else:
+                master = os.path.join(imageHome, path)
+                mfbd2fbs = False
+        if path.find(ref_slave) != -1:
+            if i[-1] == '3':
+                slave = os.path.join(imageHome, path)
+                sfbd2fbs = True
+            else:
+                slave = os.path.join(imageHome, path)
+                sfbd2fbs = False
+
+    with open(refgeo+'/insarApp.xml', 'w') as f:
+        f.write(xml_tmp2().format(masterIMG=imagedata('IMG', master, polor), masterLED=imagedata('LED', master, polor), masterDate=ref_master[2:],
+                                  slaveIMG=imagedata('IMG', slave, polor), slaveLED=imagedata('LED', slave, polor), slaveDate=ref_slave[2:],
+                                  DEM=DEM, unwrapMethod=unwrapMethod, mFBD2FBS=fbd2fbs(mfbd2fbs), sFBD2FBS=fbd2fbs(sfbd2fbs)))
+    bash = open('02_geobox.sh', 'w')
+    bash.write('cd %s\n' % refgeo)
+    bash.write('insarApp.py insarApp.xml --steps\n')
+    bash.write('echo " - find geobox completed. -" >> %s' % log)
     bash.close()
 
-def draw(imageDate, tableTS, time, space): #draw the pairs within Time-Space baseline
-    day = {}
-    with open(imageDate, 'r') as f:
-        dates = f.read().strip().split()
-        ref_date = dates[0]
 
-    with open(tableTS, 'r') as f:
-        tb = f.readlines()
-        for i in range(len(tb)):
-            if i > 0:
-                line = tb[i].split()
-                dates = line[0].split('_')
-                date1 = datetime(int('20'+dates[0][:2]), int(dates[0][2:4]), int(dates[0][4:6]))
-                date2 = datetime(int('20'+dates[1][:2]), int(dates[1][2:4]), int(dates[1][4:6]))
-                perp_bl_ave = float(line[3])
-                if int('20'+dates[0]) == int(ref_date):
-                    day[date1] = 0
-                    day[date2] = perp_bl_ave
-        for i in range(len(tb)):
-            if i > 0:
-                line = tb[i].split()
-                dates = line[0].split('_')
-                date1 = datetime(int('20'+dates[0][:2]), int(dates[0][2:4]), int(dates[0][4:6]))
-                date2 = datetime(int('20'+dates[1][:2]), int(dates[1][2:4]), int(dates[1][4:6]))
-                diff_date = (date2-date1).days
-                perp_bl_ave = float(line[3])
-                if abs(perp_bl_ave) <= space and diff_date <= time:
-                    plt.plot([date1,date2],[day[date1], day[date2]], 'b-')
-    X = day.keys()
-    Y = [day[i] for i in X]
-    plt.plot(X,Y,'o', label='ALOS image')
-    plt.legend(loc='lower right')
-    plt.title('Time-Space Baseline (base on 2007.08.18)')
-    plt.xlabel('year')
-    plt.ylabel('perpendicular baseline (m)')
-    plt.show()
+def log(logfile):
+    os.system('echo "  = = = = = = = = = = = = = = = = = = = = = = = = = = =" > runISCE.log')
+    os.system('echo "  |                                                   |" >> runISCE.log')
+    os.system('echo "  |                                                   |" >> runISCE.log')
+    os.system('echo "  |               - ISCE InSAR Project -              |" >> runISCE.log')
+    os.system('echo "  |                                                   |" >> runISCE.log')
+    os.system('echo "  |                                                   |" >> runISCE.log')
+    os.system('echo "  = = = = = = = = = = = = = = = = = = = = = = = = = = =" >> runISCE.log')
 
-def ifglist(ilist):
-    pairs = os.listdir('isce_out')
-    ifglist = open(ilist, 'w')
-    for pair in pairs:
-        dates = pair.split('_')
-        date1 = '20' + dates[0][:2] + dates[0][2:4] + dates[0][4:6]
-        date2 = '20' + dates[1][:2] + dates[1][2:4] + dates[1][4:6]
-        with open('isce_out/'+pair+'/isce.log', 'r') as f:
-        #with open('/home/willie/DATA/isce_output/'+pair+'/isce.log', 'r') as f:
-            for line in f:
-                if 'baseline.perp_baseline_bottom' in line:
-                    perp_bl_b = line[32:].strip()
-                elif 'baseline.perp_baseline_top' in line:
-                    perp_bl_t = line[29:].strip()
-                    break
-        perp_bl_ave = (float(perp_bl_b)+float(perp_bl_t)) / 2
-        ifglist.write('%-10s%-10s%-12.4f%-4s\n' %(date1, date2, perp_bl_ave, 'ALOS'))
-    ifglist.close()
 
 if __name__ == '__main__':
     root = os.getcwd()
-    #os.system('echo "- - - - ISCE InSAR Project - - - -" > runISCE.log')
-    #steps1(dataDate, imageHome, DEM, polor, unwrapMethod)
-    #runScript(root, '01_calperpbl.sh')
-    #steps2(max_daydiff, max_perpendicular_baseline)
-    #runScript(root, '02_runDInSAR.sh')
-    #draw(dataDate, 'perp_baseline.txt', max_daydiff, max_perpendicular_baseline)
-    ifglist('ifg.list')
+    log = os.path.join(root, 'runISCE.log')
+    #preproc(dataDate, imageHome, DEM, polor)
+    #runscript(root, '01_calBperp.sh')
+    calbperp('Bperp', dataDate)
+    geobox('TStable.txt', imageHome, DEM, polor, unwrapMethod)
+    runscript(root, '02_geobox.sh')
